aa800670266edd96f68add19a8cebcdbfc1e474b
Raqueline Gomes <55905389+raquelinevr@users.noreply.github.com>
2020-10-18 13:49:22 -0300

    Update and rename ATIVIDADE 3.txt to ATIVIDADE 3.py

--- Avaliaçoes/ATIVIDADE 3.py
+++ Avaliaçoes/ATIVIDADE 3.py
@@ -3,6 +3,16 @@ Faça um programa que leia a altura e o sexo (M-masculino, F-feminino) de uma pe
 
 para homens: (72.7 * altura) - 58.0
 para mulheres: (62.1 * altura) - 44.7
+
+altura=float(input('Informe a altura: '))
+sexo=int(input('Informe o sexo:\n1-Feminino\n2-Masculino\n'))
+pf=(72.7*altura) - 58.0
+pm=(62.1*altura) - 44.7
+if sexo==1:
+    print('Seu peso ideal é de: ',pf)
+elif sexo==2:
+    print('Seu peso ideal é de: ',pm)
+    
  
 QUESTÃO 2:
 Recomendam-se estudantes para bolsas de estudo em função de seu desempenho.
@@ -11,6 +21,16 @@ A natureza das recomendações é baseada na seguinte tabela:
  B OU C - RECOMENDADO 
  D - NÃO RECOMENDADO 
 Faça um programa que leia o nome e o conceito de um estudante e exiba o nome do estudante e sua respectiva recomendação.
+
+nome=input('Informe seu nome: ')
+conceito=input('Informe seu conceito: A, B ou C, D: ')
+if conceito == 'A':
+    print('Olá',nome, 'sua recomendação foi: "Fortemente recomendado" ')
+elif conceito == 'B' or conceito =='C':
+    print('Olá',nome, 'sua recomendação foi: "Recomendado" ')
+elif conceito == 'D':
+    print('Olá',nome, 'sua recomendação foi: "Não recomendado" ')
+
 
 QUESTÃO 3:
 Uma determinada loja está oferecendo um desconto para os seus clientes, de acordo com o valor total de sua compra. O valor do desconto é determinado pela tabela abaixo:
@@ -24,6 +44,17 @@ leia o valor da compra de um cliente;
 calcule e mostre o valor do seu desconto;
 calcule e mostre o valor final a pagar (valor da compra - desconto).
 
+valorc=float(input('Informe o valor da compra: R$ '))
+y=valorc-5.00
+z=valorc-20.00
+if valorc >=0.00 and valorc<=49.99:
+    print('O valor final a pagar é: R$',valorc, 'Não houve desconto. ')
+elif valorc >= 50.00 and  valorc<= 99.99:
+    print('Sua compra foi: R$',valorc, 'Você teve um desconto de R$5,00 e o valor final a pagar foi: R$',y)
+elif valorc >=100.00:
+    print('Sua compra foi: R$',valorc, 'Você teve um desconto de R$20,00 e o valor final a pagar foi: R$',z)
+
+
 QUESTÃO 4:
 Numa fábrica trabalham homens e mulheres divididos em duas classes:
 
@@ -35,41 +66,6 @@ Escreva um programa que leia a matrícula do operário e o número de peças fab
 Como resposta mostre a matrícula do operário, sua classe e o salário que ele deve receber.
 
 
-
-RESPOSTAS:
-1.
-altura=float(input('Informe a altura: '))
-sexo=int(input('Informe o sexo:\n1-Feminino\n2-Masculino\n'))
-pf=(72.7*altura) - 58.0
-pm=(62.1*altura) - 44.7
-if sexo==1:
-    print('Seu peso ideal é de: ',pf)
-elif sexo==2:
-    print('Seu peso ideal é de: ',pm)
-    
-2.
-nome=input('Informe seu nome: ')
-conceito=input('Informe seu conceito: A, B ou C, D: ')
-if conceito == 'A':
-    print('Olá',nome, 'sua recomendação foi: "Fortemente recomendado" ')
-elif conceito == 'B' or conceito =='C':
-    print('Olá',nome, 'sua recomendação foi: "Recomendado" ')
-elif conceito == 'D':
-    print('Olá',nome, 'sua recomendação foi: "Não recomendado" ')
-
-
-3.
-valorc=float(input('Informe o valor da compra: R$ '))
-y=valorc-5.00
-z=valorc-20.00
-if valorc >=0.00 and valorc<=49.99:
-    print('O valor final a pagar é: R$',valorc, 'Não houve desconto. ')
-elif valorc >= 50.00 and  valorc<= 99.99:
-    print('Sua compra foi: R$',valorc, 'Você teve um desconto de R$5,00 e o valor final a pagar foi: R$',y)
-elif valorc >=100.00:
-    print('Sua compra foi: R$',valorc, 'Você teve um desconto de R$20,00 e o valor final a pagar foi: R$',z)
-
-4.
 matriculaop=int(input('Informe sua matricula: '))
 np=int(input('Informe o numero de peças que você fabricou esse mês: '))
 salario=1045
@@ -77,7 +73,7 @@ b=np-30
 x=salario+b*10
 
 if np <=30:
-    print('Olá',matriculaop, 'sua classe foi A e seu salario foi: R$1.045')
+    print('Olá',matriculaop, 'sua classe foi A e seu salario foi: ',salario)
 elif np >=30:
     print('Olá',matriculaop, 'sua classe foi B e seu salario foi: R$',x)
 
